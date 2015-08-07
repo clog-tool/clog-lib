64be596d9369daf55184a7038d3899e4572adba4
Kevin K <kbknapp@gmail.com>
2015-08-07 10:30:51 -0400

    docs: rebuilt docs for lib specific repo

--- docs/clog/clog/sidebar-items.js
+++ docs/clog/clog/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["LinkStyle",""]],"struct":[["Clog","The base struct used to set options and interact with the library."]]});
+initSidebarItems({"struct":[["Clog","The base struct used to set options and interact with the library."]]});
