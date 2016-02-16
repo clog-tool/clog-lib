b6c870ea2585fd49c4d6bc4f2175366139269c98
Jan-Erik Rediger <janerik@fnordig.de>
2016-02-16 09:18:54 +0100

    refactor: Remove dependency on regex_macro
    It was not used in stable at all, because it only works in nightly.
    Now that regex! is almost always slower¹ there's no reason to keep it in.
    
    ¹: https://github.com/rust-lang-nursery/regex/pull/164

--- src/macros.rs
+++ src/macros.rs
@@ -13,9 +13,6 @@ macro_rules! werr(
     })
 );
 
-// regex cheat thanks to https://github.com/BurntSushi
-// Until regex_macros compiles with nightly again, this directive should be commented out
-// #[cfg(not(unstable))]
 macro_rules! regex(
     ($s:expr) => (::regex::Regex::new($s).unwrap());
 );
